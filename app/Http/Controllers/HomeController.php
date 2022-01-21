82538c17ff3a0a9568ecd79ec8b08b4377c6659e
LuigiRagusa95 <luigiragusa99@gmail.com>
2022-01-21 14:51:07 +0100

    select all movies from database

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Movie;
 
 class HomeController extends Controller
 {
     //
+    public function index()
+    {
+        $data = Movie::all();
+        return view('pages.home', compact('data'));
+    }
 }
