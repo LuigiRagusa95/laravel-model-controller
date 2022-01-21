0a39af1220401e303b4b4505d77049ac31540ca8
LuigiRagusa95 <luigiragusa99@gmail.com>
2022-01-21 14:44:17 +0100

    init db and setup model and controller for home

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>@yield('title', 'imdb')</title>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('./css/app.css') }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <main>@yield('content')</main>
+</body>
+</html>
