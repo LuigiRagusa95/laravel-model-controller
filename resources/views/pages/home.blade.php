e966c5b5669b52cb94f10bc89cbf45283a1423c9
LuigiRagusa95 <luigiragusa99@gmail.com>
2022-01-21 15:10:10 +0100

    add a little bit of style to movie card

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,10 +2,12 @@
 @section('content')
     <ul>
         @foreach ($movies as $movie)
-            <li>
-                <h2>{{ $movie->original_title }}</h2>
-                <time>{{ $movie->date }}</time>
-                <div>{{ $movie->vote }}</div>
+            <li class="movie-card">
+                <h2 class="movie-card-title">{{ $movie->title }}</h2>
+                <div class="movie-card-vote"><strong>Vote: </strong>{{ $movie->vote }}</div>
+                <div class="movie-card-date"><strong>Date: </strong><time>{{ $movie->date }}</time></div>
+                <div class="movie-card-nationality"><strong>Nationality: </strong>{{ $movie->nationality }}</div>
+                <div class="movie-card-original-title"><strong>Original title: </strong>{{ $movie->original_title }}</div>
             </li>
             @if (!$loop->last) <hr> @endif
         @endforeach
